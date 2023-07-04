f9db0c1e23abf7d814d5ca6077491002599fb5e7
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-04 11:38:33 -0400

    adding firebase intitiation files

---
+++ src/services.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfD-3-RQH7p29oIEyhhkxMnb571IpGbbw",
+  authDomain: "calculator-2fb3f.firebaseapp.com",
+  projectId: "calculator-2fb3f",
+  storageBucket: "calculator-2fb3f.appspot.com",
+  messagingSenderId: "327496311510",
+  appId: "1:327496311510:web:b66ee2ca5df0643d679f77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

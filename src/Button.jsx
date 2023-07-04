43cc4a66a1c901f2884fd20f7a266dfeb8b0eee1
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-04 14:17:53 -0400

    Updated packages

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,18 +3,18 @@ import { handleClick } from './calculatorLogic'
 
 const Button = (props) => {
 
-  const clicked= () => {
+  const clicked = () => {
     handleClick(props.obj, props.display, props.setDisplay, props.equation, props.setEquation)
   }
-      
-    return(
-        <button className="buttons"
-                id={props.obj.id}
-                number={props.obj.displayValue}
-                action={props.obj.action}
-                onClick={clicked}>{props.obj.displayValue}
-        </button>
-    );
-  }
 
-  export default Button
+  return (
+    <button className="buttons"
+      id={props.obj.id}
+      number={props.obj.displayValue}
+      action={props.obj.action}
+      onClick={clicked}>{props.obj.displayValue}
+    </button>
+  );
+}
+
+export default Button
